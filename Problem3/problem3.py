f71ebb0b1fd0b8a5431335ce2c192414330d0052
Natalia Bustamante Rodriguez <natisaxo@gmail.com>
2021-01-15 16:03:07 +0100

    Adding comments to problems 3 and 9

--- Problem3/problem3.py
+++ Problem3/problem3.py
@@ -4,6 +4,7 @@ factor = 2
 while factor * factor <= num:
     while num % factor == 0:
         num = num / factor
+        #in case num is a perfect square:
         if num / factor == 1:
             print(num)
     factor += 1
--- Problem9/problem9.py
+++ Problem9/problem9.py
@@ -2,6 +2,9 @@ import math
 
 perimeter = 1000
 
+#Euclid's formula: given an arbitrary pair of integers m and n  with m > n > 0, 
+#the integers a = m^2-n^2, b = 2mn, c = m^2+n^2 form a Pythagorean triple
+
 for m in range(1, math.ceil(math.sqrt(perimeter))):
     for n in range(1, m):
         a = m**2 - n**2
